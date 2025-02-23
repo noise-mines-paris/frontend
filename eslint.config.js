ddedb2363fbcbd44854846a6815c2257729dfd9b
David Bretaud <david.bretaud.dev@gmail.com>
2025-02-23 19:11:56 +0100

    style(eslint): Implement eslint config

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+	{
+		languageOptions: { globals: globals.browser },
+		settings: {
+			// Specify a react version just to disable a eslint-plugin-react warning about
+			// missing react version
+			react: {
+				version: '19.0.0',
+			},
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	eslintConfigPrettier, // Disable some eslint rules to prevent conflicts with prettier
+	{
+		files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+		rules: {
+			// Disable the following line as we can't import React (we are using preact)
+			'react/react-in-jsx-scope': 'off',
+		},
+	},
+	{
+		// Ignore the following folders and files when running eslint
+		ignores: ['node_modules/', 'dist', 'build'],
+	},
+];
--- src/app.tsx
+++ src/app.tsx
@@ -9,15 +9,15 @@ export function App() {
 	return (
 		<>
 			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} class="logo" alt="Vite logo" />
+				<a href="https://vite.dev" target="_blank" rel="noreferrer">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
-				<a href="https://preactjs.com" target="_blank">
-					<img src={preactLogo} class="logo preact" alt="Preact logo" />
+				<a href="https://preactjs.com" target="_blank" rel="noreferrer">
+					<img src={preactLogo} className="logo preact" alt="Preact logo" />
 				</a>
 			</div>
 			<h1>Vite + Preact</h1>
-			<div class="card">
+			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
@@ -30,12 +30,13 @@ export function App() {
 				<a
 					href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
 					target="_blank"
+					rel="noreferrer"
 				>
 					create-preact
 				</a>
 				, the official Preact + Vite starter
 			</p>
-			<p class="read-the-docs">
+			<p className="read-the-docs">
 				Click on the Vite and Preact logos to learn more
 			</p>
 		</>
