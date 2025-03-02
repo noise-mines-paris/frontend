51db7c212a13e34654f21b562650d550af5f1a5b
David Bretaud <david.bretaud.dev@gmail.com>
2025-03-02 19:53:20 +0100

    feat(tailwindcss): Setup tailwind css V4 for css management

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'preact/hooks';
 import preactLogo from './assets/preact.svg';
 import viteLogo from '/vite.svg';
-import './app.css';
+import { Button } from './components/Button';
 
 export function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
+		<main className="bg-white">
 			<div>
 				<a href="https://vite.dev" target="_blank" rel="noreferrer">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,9 +18,10 @@ export function App() {
 			</div>
 			<h1>Vite + Preact</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
+				<Button
+					onClick={() => setCount((count) => count + 1)}
+					label={`Count is ${count}`}
+				/>
 				<p>
 					Edit <code>src/app.tsx</code> and save to test HMR
 				</p>
@@ -39,6 +40,6 @@ export function App() {
 			<p className="read-the-docs">
 				Click on the Vite and Preact logos to learn more
 			</p>
-		</>
+		</main>
 	);
 }
